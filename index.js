8d846ea8b0ed684e8e9458771e07ddd9dbcba97a
Vinicius Lima <viniciuslima76@outlook.com.br>
2018-08-24 21:41:15 -0300

    database first access

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/times', (req, res) => {
-    let result = ''
-    const times = process.env.TIMES || 5;
-    for (i = 0; i < times; i++) {
-      result += i + ' ';
+  .get('/', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM ACCOUNT');
+      res.send(result.rows);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
     }
-    res.send(result)
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
