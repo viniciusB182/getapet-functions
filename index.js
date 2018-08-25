cfabe1be049960d763f5a04a8e4f37fdba9c31af
Vinicius Lima <viniciuslima76@outlook.com.br>
2018-08-24 21:52:44 -0300

    return account list from postgre container

--- index.js
+++ index.js
@@ -2,23 +2,27 @@ const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const { Pool } = require('pg');
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
 });
+
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', async (req, res) => {
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/account', async (req, res) => {
     try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM ACCOUNT');
-      res.send(result.rows);
-      client.release();
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM ACCOUNT');
+        res.send(result.rows);
+        client.release();
     } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
+        console.error(err);
+        res.send("Error " + err);
     }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+});
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
